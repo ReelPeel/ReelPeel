b920ad1e4db4187cc1f2c6636516bc4d41d010e4
alisondur <alison.durst@stud.uni-regensburg.de>
2026-01-03 18:14:40 +0000

    evaluation draft

---
+++ app2/test_configs/raw_eval_config.py
@@ -0,0 +1,18 @@
+from app.preprompts import PROMPT_TMPL_S7
+
+RAW_PIPELINE_CONFIG = {
+    "name": "Full_End_to_End_Run",
+    "debug": True,
+    "steps": [
+        # Step 7: Determine Truthness
+            {
+                "type": "truthness",
+                "settings": {
+                    "base_url": "http://localhost:11434/v1",
+                    "model": "gemma3:12b",
+                    "prompt_template": PROMPT_TMPL_S7
+                }
+            },
+
+    ]
+}
